9d0ff765fa6399a0c202dbaa410e9542d62068ab
Ajaydama <103327237+Ajaydama@users.noreply.github.com>
2022-08-15 16:00:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,58 +1,65 @@
-from flask import Flask ,render_template,request
-from flask.helpers import flash
-from flask_wtf import FlaskForm
-from six import print_
-from wtforms import StringField
-from wtforms.fields.simple import FileField
-from wtforms.validators import DataRequired
-from wtforms.fields import FileField
-from flask_wtf.file import FileAllowed, FileRequired
-from prediction import prediction
-from PIL import Image
-import secrets
+from __future__ import division, print_function
+
 import os
+import numpy as np
+
+import cv2
+from keras.models import load_model
+from keras import utils
+
+from flask import Flask, request, render_template
+from werkzeug.utils import secure_filename
 
 
-class MRIFileForm(FlaskForm):
-    file = FileField("MRI image",validators=[FileRequired(),FileAllowed(["jpg","jpeg","png"])])
+app = Flask(__name__)
 
-app = Flask(__name__) 
-app.config['SECRET_KEY'] = '123456789'
-# app is routed 
-def save_picture(file):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(file.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join('./static/TI/',picture_fn)
-    i = Image.open(file)
-    i.save(picture_path)
-    print(picture_path)
-    return picture_fn
 
-@app.route('/',methods=["GET","POST"])
-def display():
-    form = MRIFileForm()
-    if request.method == "GET":
-        return render_template("index.html",form=form)
-    elif request.method == "POST":
-        if form.validate_on_submit():
-            file = form.file.data
-            saved_picture = save_picture(file)
-            result = prediction(saved_picture)
-            if result == "no":
-                alert = "success"
-                flash('No tumor has been Detected!!',alert)
-            elif result == "yes":
-                alert="danger"
-                flash('Tumor has been Detected!!',alert)
-            return render_template('index.html',form=form)
+print('Model loaded. Check http://127.0.0.1:5000/home')
+
+def model_predict(img_path):
+        model=load_model('Food.h5')
+        img = utils.load_img(img_path, target_size=(64 ,64))
+        x = utils.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        preds=np.argmax(model.predict(x), axis=1)
+        if(preds==0):
+            a="https://www.thespruceeats.com/thmb/QUyxKfZoo6rHgczWZieyQBAKviw=/1333x1000/smart/filters:no_upscale()/how-to-make-french-fries-995932_final-6a36c7cf85604fa9b15fc868a7bcc6c8.png";
+    
+        elif(preds==1):
+            a="https://files.liveworksheets.com/def_files/2020/10/3/1003050249676828/1003050249676828001.jpg";
+        else:
+            a="https://i.pinimg.com/736x/8d/ce/56/8dce569d41b2d6dfb88c48dd698c195e.jpg";
+        return a
+            
+   
+        
+
+@app.route('/home', methods=['GET'])
+def index():
+    # Main page
+    return render_template('home.html')
+@app.route('/prediction', methods=['GET'])
+def index2():
+    # Main page
+    return render_template('launch.html')
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    print("hi")
+    if request.method == 'POST':
+        f = request.files['fileq']
+       
+
+      
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+        result=model_predict(file_path)
+    
+    return render_template('launchr.html',user_image=result)
+    return None
 
 
 if __name__ == '__main__':
-    app.run(debug = True) 
-
-
-
-
-
-
+    app.run(debug=True)
+    
